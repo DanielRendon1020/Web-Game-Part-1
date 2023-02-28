50bdff0916d20c9cbb1af5977397b24f3e530d27
DanielRendon1020 <d.danny1020@gmail.com>
2023-02-27 17:26:13 -0800

    event listener click to remove

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function newImage(asset, source, posLeft, posBottom){
     newElem.style.position = 'fixed'
     newElem.style.left = posLeft
     newElem.style.bottom = posBottom
+    newElem.addEventListener('click', function(){
+        newElem.remove()
+    })
     document.body.append(newElem)
     return(asset)
 }
@@ -15,3 +18,8 @@ newImage('tree', 'tree.png', '200px', '300px')
 newImage('pillar', 'pillar.png', '350px', '100px')
 newImage('crate', 'crate.png', '150px', '200px')
 newImage('well', 'well.png', '500px', '425px')
+newImage('sword', 'sword.png', '500px', '405px')
+newImage('shield', 'sheild.png', '165px', '185px')
+newImage('staff', 'staff.png', '600px', '100px')
+
+
