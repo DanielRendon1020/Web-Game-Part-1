8d2deb6faa2fe68e5e293ad8c876a221b9067515
DanielRendon1020 <d.danny1020@gmail.com>
2023-02-27 17:04:01 -0800

    working function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(asset, source, posLeft, posBottom){
+    let newElem = document.createElement('img')
+    newElem.src = `assets/${source}`
+    newElem.style.position = 'fixed'
+    newElem.style.left = posLeft
+    newElem.style.bottom = posBottom
+    document.body.append(newElem)
+    return(asset)
+}
+
+newImage('greenCharacter', 'green-character.gif', '100px', '100px');
