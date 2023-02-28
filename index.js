87d34c082eb04278c3cf91dbfd7a41326c3a5859
DanielRendon1020 <d.danny1020@gmail.com>
2023-02-27 17:14:43 -0800

    all items with function

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function newImage(asset, source, posLeft, posBottom){
 }
 
 newImage('greenCharacter', 'green-character.gif', '100px', '100px');
+newImage('pineTree', 'pine-tree.png', '450px', '200px')
+newImage('tree', 'tree.png', '200px', '300px')
+newImage('pillar', 'pillar.png', '350px', '100px')
+newImage('crate', 'crate.png', '150px', '200px')
+newImage('well', 'well.png', '500px', '425px')
